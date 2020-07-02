d2374081a4c333313677e3daae3505a90fc6f801
tmhernan <tiffany.hernandez@asu.edu>
2020-07-02 15:51:24 -0700

    Nav Bar

---
+++ src/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './NavBar.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+      
+      </div>
+
+      <div className='Nav'>
+        <ul id='nav-links'>
+          <li>
+            <a href='www.a.com'>Home</a>
+          </li>
+          <li>
+            <a href='www.a.com'>Favorites</a>
+          </li>
+          <li>
+            <a href='www.a.com'>About</a>
+          </li>
+          <li>
+            <a href='www.a.com'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
